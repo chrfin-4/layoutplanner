5bcd174b44dd879939c54f5f5d9bfa8b19500497
Christoffer Fink <chrfin-4@student.ltu.se>
2020-07-07 07:10:05 +0200

    Moving from a single surface to a wagon with many

--- src/main/java/se/ltu/kitting/model/Wagon.java
+++ src/main/java/se/ltu/kitting/model/Wagon.java
@@ -1,9 +1,12 @@
 package se.ltu.kitting.model;
 
+import java.util.Collection;
 import java.util.List;
+import static java.util.stream.Collectors.toList;
 
 // Same properties in request and response, but different subsets are required.
 public class Wagon {
+
   // Required in request and response.
   private String wagonId;
   private List<String> capabilities;
@@ -12,10 +15,49 @@ public class Wagon {
   private List<Surface> surfaces;
 
   // Optional.
+  // TODO: derive from surfaces? What is this used for?
   private Dimensions dimensions;
+
+
+
+  public static Wagon of(Surface surface) {
+    return new Wagon("0", List.of(surface));
+  }
+
+  public static Wagon of(List<Surface> surfaces) {
+    return new Wagon("0", surfaces);
+  }
+
+  public static Wagon of(String id, List<Surface> surfaces) {
+    return new Wagon(id, surfaces);
+  }
+
+  private Wagon(String id, List<Surface> surfaces) {
+    this.wagonId = id;
+    this.surfaces = surfaces;
+  }
+
+  public String wagonId() {
+    return wagonId;
+  }
+
+  public Collection<String> capabilities() {
+    return capabilities;
+  }
 
   public List<Surface> surfaces() {
     return surfaces;
+  }
+
+  public Surface surfaceOf(Dimensions position) {
+    return surfaces.stream().filter(s -> s.origin.z == position.z).findAny().get();
+  }
+
+  public List<Dimensions> allPositions() {
+    return surfaces.stream()
+      .map(Surface::getSurfacePositions)
+      .flatMap(Collection::stream)
+      .collect(toList());
   }
 
 }
