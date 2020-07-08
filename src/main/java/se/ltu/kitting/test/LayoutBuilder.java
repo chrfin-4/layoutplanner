8413ad790e512b0b4c1c78daf952eb243ed6ff1a
Christoffer Fink <chrfin-4@student.ltu.se>
2020-07-08 10:58:24 +0200

    Updated LayoutExamples to use new LayoutBuilder

---
+++ src/main/java/se/ltu/kitting/test/LayoutBuilder.java
@@ -0,0 +1,208 @@
+package se.ltu.kitting.test;
+
+import java.util.*;
+import se.ltu.kitting.model.*;
+
+// TODO: rename to something like KitBuilder?
+/**
+ * Tool for building layouts (and the parts that should be added to them).
+ * <strong>Note that this a very early, simple, incomplete version.</strong>
+ * When adding multiple surfaces by only giving their dimensions, assigns
+ * them origins so they are stacked starting with the first at the bottom.
+ * @author Christoffer Fink
+ */
+public class LayoutBuilder {
+
+  private final PartBuilder pb;
+  private final List<Part> parts = new ArrayList<>();
+  private final List<Surface> surfaces = new ArrayList<>();
+  private String wagonId = "wagon";
+  private final Set<Integer> ids = new HashSet<>();
+
+  private int counter = 1;
+
+  private LayoutBuilder() {
+    pb = new PartBuilder(this);
+  }
+
+  public Layout build() {
+    Wagon wagon = Wagon.of(wagonId, List.copyOf(surfaces));
+    return new Layout(wagon, List.copyOf(parts));
+  }
+
+  public static LayoutBuilder builder() {
+    return new LayoutBuilder();
+  }
+
+  public LayoutBuilder surfaces(Dimensions ... sizes) {
+    Arrays.stream(sizes).forEach(this::surface);
+    return this;
+  }
+
+  public LayoutBuilder surfaces(Surface ... surfaces) {
+    Arrays.stream(surfaces).forEach(this::surface);
+    return this;
+  }
+
+  public LayoutBuilder surface(int width, int depth, int height) {
+    return surface(Dimensions.of(width, depth, height));
+  }
+
+  public LayoutBuilder surface(Dimensions size) {
+    return surface(Surface.of(nextSurfaceOrigin(), size));
+  }
+
+  public LayoutBuilder surface(Surface surface) {
+    if (!abovePreviousSurface(surface)) {
+      throw new IllegalArgumentException("Surface collides with the one below");
+    }
+    surfaces.add(surface);
+    return this;
+  }
+
+  public PartBuilder part() {
+    return pb.id(counter);
+  }
+
+  public LayoutBuilder parts(Part ... parts) {
+    Arrays.stream(parts).forEach(this::part);
+    return this;
+  }
+
+  public LayoutBuilder parts(Dimensions ... sizes) {
+    Arrays.stream(sizes).forEach(this::part);
+    return this;
+  }
+
+  public LayoutBuilder part(int width, int depth, int height) {
+    return part(Dimensions.of(width, depth, height));
+  }
+
+  public LayoutBuilder part(Dimensions size) {
+    //return part(new Part(-1, "?", size));
+    //return part().id(counter++).dimensions(size).add();
+    return part().id(counter).dimensions(size).add();
+  }
+
+  // All parts are ultimately added by this method.
+  public LayoutBuilder part(Part part) {
+    int id = part.getId();
+    if (ids.contains(id)) {
+      throw new IllegalArgumentException("Illegal argument: " + id);
+    }
+    ids.add(id);
+    counter = ids.stream().max(Integer::compare).get() + 1;
+    parts.add(part);
+    pb.id(counter);
+    return this;
+  }
+
+  // Does the previously added surface (if any) end below this new surface?
+  // FIXME: needs to check for a 0 height that means it's the top surface, with
+  // unlimited height.
+  private boolean abovePreviousSurface(Surface surface) {
+    int nextZ = surface.origin.z;
+    int prevZ = prevSurface().map(s -> s.origin.z + s.dimensions.z).orElse(0);
+    return prevZ <= nextZ;
+  }
+
+  private Dimensions nextSurfaceOrigin() {
+    return prevSurface().map(s -> s.origin.plus(s.dimensions)).orElse(Dimensions.ZERO);
+  }
+
+  private Optional<Surface> prevSurface() {
+    int size = surfaces.size();
+    if (size == 0) {
+      return Optional.empty();
+    }
+    return Optional.of(surfaces.get(size-1));
+  }
+
+  /**
+   * Build parts.
+   * Some defaults:
+   * - By default allows all sides.
+   * - Preferes bottom if no preferred side set.
+   */
+  public static class PartBuilder {
+    private final LayoutBuilder lb;
+    private int id;
+    private String pnr;
+    private Dimensions dimensions;
+    private Side preferredSide;
+    private Rotation rotation;
+    private Set<Side> allowedSides;
+    private Set<Side> disallowedSides;
+
+    private PartBuilder(LayoutBuilder lb) {
+      this.lb = lb;
+    }
+
+    public LayoutBuilder add() {
+      Part part = new Part(id, pnr, dimensions);
+      // TODO: set additional properties
+      return lb.part(part);
+    }
+
+    public LayoutBuilder add(int count) {
+      while (count-- > 0) {
+        add();
+      }
+      return lb;
+    }
+
+    public PartBuilder id(int id) {
+      this.id = id;
+      return this;
+    }
+
+    public PartBuilder partNumber(String pnr) {
+      this.pnr = pnr;
+      return this;
+    }
+
+    public PartBuilder dimensions(int x, int y, int z) {
+      return dimensions(Dimensions.of(x, y, z));
+    }
+
+    public PartBuilder dimensions(Dimensions size) {
+      this.dimensions = size;
+      return this;
+    }
+
+    public PartBuilder preferredSide(Side side) {
+      this.preferredSide = side;
+      return allowSide(side);
+    }
+
+    public PartBuilder allowSides(Side ... sides) {
+      Arrays.stream(sides).forEach(this::allowSide);
+      return this;
+    }
+
+    public PartBuilder allowSides(Collection<Side> sides) {
+      sides.stream().forEach(this::allowSide);
+      return this;
+    }
+
+    public PartBuilder allowSide(Side side) {
+      allowedSides.add(side);
+      return this;
+    }
+
+    public PartBuilder rotation(int z) {
+      return rotation(Rotation.of(z));
+    }
+
+    public PartBuilder rotation(Rotation rotation) {
+      this.rotation = rotation;
+      return this;
+    }
+
+    public Part build() {
+      throw new UnsupportedOperationException("Not implemented.");
+    }
+
+  }
+
+}
--- src/main/java/se/ltu/kitting/test/LayoutExamples.java
+++ src/main/java/se/ltu/kitting/test/LayoutExamples.java
@@ -32,7 +32,7 @@ public class LayoutExamples {
 		return layouts;
 	}
 
-	public static String layoutDescription(int index){
+	public static String layoutDescription(int index) {
 		if(index >= layouts.size()){
 			throw new IndexOutOfBoundsException("Layout " + index + " does not exist");
 		}
@@ -52,181 +52,164 @@ public class LayoutExamples {
 
 	/* ---- Possible solutions ---- */
 
-	public static Layout layout0(){
-		List<Part> parts = new ArrayList<>();
-		parts.add(new Part(1,1,new Dimensions(5,10,1)));
-		parts.add(new Part(2,1,new Dimensions(10,5,1)));
-		Surface surface = Surface.of(10,10,1);
-		Layout layout = new Layout(surface, parts);
-		return layout;
-	}
+  public static Layout layout0() {
+    return LayoutBuilder.builder()
+      .surface(10,10,1)
+      .part(5,10,1)
+      .part(10,5,1)
+      .build();
+  }
 
 	/* NOTE: Only for Z90 rotation */
 
 	// Rotation needed, covers surface completely
-	public static Layout layout1(){
-		List<Part> parts = new ArrayList<>();
-		parts.add(new Part(1,1,new Dimensions(5,10,1)));
-		parts.add(new Part(2,1,new Dimensions(10,5,1)));
-		Surface surface = Surface.of(10,10,1);
-		Layout layout = new Layout(surface, parts);
-		return layout;
-	}
+  public static Layout layout1() {
+    return LayoutBuilder.builder()
+      .surface(10,10,1)
+      .part(5,10,1)
+      .part(10,5,1)
+      .build();
+  }
 
 	// Need to be placed in other order than given, 100 points empty, should not rotate
-	public static Layout layout2(){
-		List<Part> parts = new ArrayList<>();
-		parts.add(new Part(1,1,new Dimensions(20,10,1)));
-		parts.add(new Part(2,1,new Dimensions(30,5,1)));
-		parts.add(new Part(3,1,new Dimensions(10,15,1)));
-		Surface surface = Surface.of(30,20,1);
-		Layout layout = new Layout(surface, parts);
-		return layout;
+	public static Layout layout2() {
+    return LayoutBuilder.builder()
+      .surface(30,20,1)
+      .part(20,10,1)
+      .part(30,5,1)
+      .part(10,15,1)
+      .build();
 	}
 
 	// Easy to place, 250 points empty, small surface
-	public static Layout layout3(){
-		List<Part> parts = new ArrayList<>();
-		parts.add(new Part(1,1,new Dimensions(15,10,1)));
-		parts.add(new Part(2,1,new Dimensions(10,10,1)));
-		parts.add(new Part(3,1,new Dimensions(20,5,1)));
-		Surface surface = Surface.of(30,20,1);
-		Layout layout = new Layout(surface, parts);
-		return layout;
+	public static Layout layout3() {
+    return LayoutBuilder.builder()
+      .surface(30,20,1)
+      .part(15,10,1)
+      .part(10,10,1)
+      .part(20,5,1)
+      .build();
 	}
 
 	// 100 squares fit perfectly
-	public static Layout layout4(){
-		List<Part> parts = new ArrayList<>();
-		for(int i = 0; i < 100; i++){
-			parts.add(new Part(i+1,1,new Dimensions(10,10,1)));
-		}
-		Surface surface = Surface.of(100,100,1);
-		Layout layout = new Layout(surface, parts);
-		return layout;
+	public static Layout layout4() {
+    return LayoutBuilder.builder()
+      .surface(100,100,1)
+      .part().dimensions(10,10,1).add(100)
+      .build();
 	}
 
 	// Odd shapes that just barely fit perfectly
-	public static Layout layout5(){
-		List<Part> parts = new ArrayList<>();
-		parts.add(new Part(1,1,new Dimensions(35,30,1)));
-		parts.add(new Part(2,1,new Dimensions(20,45,1)));
-		parts.add(new Part(3,1,new Dimensions(45,45,1)));
-		parts.add(new Part(4,1,new Dimensions(35,60,1)));
-		parts.add(new Part(5,1,new Dimensions(50,45,1)));
-		parts.add(new Part(6,1,new Dimensions(15,55,1)));
-		parts.add(new Part(7,1,new Dimensions(85,10,1)));
-		Surface surface = Surface.of(100,100,1);
-		Layout layout = new Layout(surface, parts);
-		return layout;
+	public static Layout layout5() {
+    return LayoutBuilder.builder()
+      .surface(100,100,1)
+      .part(35,30,1)
+      .part(20,45,1)
+      .part(45,45,1)
+      .part(35,60,1)
+      .part(50,45,1)
+      .part(15,55,1)
+      .part(85,10,1)
+      .build();
 	}
 
 	// Variation of layout5, one piece removed, 2025 points empty
-	public static Layout layout6(){
-		List<Part> parts = new ArrayList<>();
-		parts.add(new Part(1,1,new Dimensions(35,30,1)));
-		parts.add(new Part(2,1,new Dimensions(20,45,1)));
-		//parts.add(new Part(1,1,new Dimensions(45,45,1)));
-		parts.add(new Part(3,1,new Dimensions(35,60,1)));
-		parts.add(new Part(4,1,new Dimensions(50,45,1)));
-		parts.add(new Part(5,1,new Dimensions(15,55,1)));
-		parts.add(new Part(6,1,new Dimensions(85,10,1)));
-		Surface surface = Surface.of(100,100,1);
-		Layout layout = new Layout(surface, parts);
-		return layout;
+	public static Layout layout6() {
+    return LayoutBuilder.builder()
+      .surface(100,100,1)
+      .part(35,30,1)
+      .part(20,45,1)
+      .part(35,60,1)
+      .part(50,45,1)
+      .part(15,55,1)
+      .part(85,10,1)
+      .build();
 	}
 
 	// Rotation needed, fits perfectly.
-	public static Layout layout7(){
-		List<Part> parts = new ArrayList<>();
-		parts.add(new Part(1,1,new Dimensions(25,100,1)));
-		parts.add(new Part(2,1,new Dimensions(75,50,1)));
-		parts.add(new Part(3,1,new Dimensions(25,75,1)));
-		parts.add(new Part(4,1,new Dimensions(75,25,1)));
-		Surface surface = Surface.of(100,100,1);
-		Layout layout = new Layout(surface, parts);
-		return layout;
+	public static Layout layout7() {
+    return LayoutBuilder.builder()
+      .surface(100,100,1)
+      .part(25,100,1)
+      .part(75,50,1)
+      .part(25,75,1)
+      .part(75,25,1)
+      .build();
 	}
 
 	// Small surface, no rotation needed, 15 points empty
-	public static Layout layout8(){
-		List<Part> parts = new ArrayList<>();
-		parts.add(new Part(1,1,new Dimensions(5,5,1)));
-		parts.add(new Part(2,1,new Dimensions(2,10,1)));
-		parts.add(new Part(3,1,new Dimensions(8,5,1)));
-		Surface surface = Surface.of(10,10,1);
-		Layout layout = new Layout(surface, parts);
-		return layout;
+	public static Layout layout8() {
+    return LayoutBuilder.builder()
+      .surface(10,10,1)
+      .part(5,5,1)
+      .part(2,10,1)
+      .part(8,5,1)
+      .build();
 	}
 
 	/* For all rotations */
 
 	// Need rotation in both X90 and Y90
-	public static Layout layout9(){
-		List<Part> parts = new ArrayList<>();
-		parts.add(new Part(1,1,new Dimensions(10,5,10)));
-		parts.add(new Part(2,1,new Dimensions(10,10,3)));
-		parts.add(new Part(3,1,new Dimensions(2,10,10)));
-		Surface surface = Surface.of(10,10,10);
-		Layout layout = new Layout(surface, parts);
-		return layout;
+	public static Layout layout9() {
+    return LayoutBuilder.builder()
+      .surface(10,10,10)
+      .part(10,5,10)
+      .part(10,10,3)
+      .part(2,10,10)
+      .build();
 	}
 
 	// Need rotation in double directions Z90Y90 and Z90X90
-	public static Layout layout10(){
-		List<Part> parts = new ArrayList<>();
-		parts.add(new Part(1,1,new Dimensions(10,5,20)));
-		parts.add(new Part(2,1,new Dimensions(5,30,3)));
-		//parts.add(new Part(1,1,new Dimensions(2,10,10)));
-		Surface surface = Surface.of(30,13,5);
-		Layout layout = new Layout(surface, parts);
-		return layout;
+	public static Layout layout10() {
+    return LayoutBuilder.builder()
+      .surface(30,13,5)
+      .part(10,5,20)
+      .part(5,30,3)
+      .build();
 	}
 
 	// "Real" example
-	public static Layout layout11(){
-		List<Part> parts = new ArrayList<>();
-		parts.add(new Part(1,1,new Dimensions(200,400,100)));
-		parts.add(new Part(2,1,new Dimensions(400,400,400)));
-		parts.add(new Part(3,1,new Dimensions(1000,100,100)));
-		parts.add(new Part(4,1,new Dimensions(200,200,200)));
-		parts.add(new Part(5,1,new Dimensions(200,300,200)));
-		parts.add(new Part(6,1,new Dimensions(100,500,300)));
-		parts.add(new Part(7,1,new Dimensions(100,250,100)));
-		parts.add(new Part(8,1,new Dimensions(100,250,100)));
-		parts.add(new Part(9,1,new Dimensions(250,150,200)));
-		parts.add(new Part(10,1,new Dimensions(350,150,200)));
-		parts.add(new Part(11,1,new Dimensions(150,200,200)));
-		parts.add(new Part(12,1,new Dimensions(150,200,200)));
-		parts.add(new Part(13,1,new Dimensions(100,250,200)));
-		parts.add(new Part(14,1,new Dimensions(200,150,100)));
-		parts.add(new Part(15,1,new Dimensions(350,100,100)));
-		Surface surface = Surface.of(1200,800,1000);
-		Layout layout = new Layout(surface, parts);
-		return layout;
+	public static Layout layout11() {
+    return LayoutBuilder.builder()
+      .surface(1200,800,1000)
+      .part(200,400,100)
+      .part(400,400,400)
+      .part(1000,100,100)
+      .part(200,200,200)
+      .part(200,300,200)
+      .part(100,500,300)
+      .part(100,250,100)
+      .part(100,250,100)
+      .part(250,150,200)
+      .part(350,150,200)
+      .part(150,200,200)
+      .part(150,200,200)
+      .part(100,250,200)
+      .part(200,150,100)
+      .part(350,100,100)
+      .build();
 	}
 
 	// "Real" example, less free space than layout 11
-	public static Layout layout12(){
-		List<Part> parts = new ArrayList<>();
-		parts.add(new Part(1,1,new Dimensions(200,400,400)));
-		parts.add(new Part(2,1,new Dimensions(400,400,400)));
-		parts.add(new Part(3,1,new Dimensions(1000,100,1000)));
-		parts.add(new Part(4,1,new Dimensions(200,200,200)));
-		parts.add(new Part(5,1,new Dimensions(200,300,300)));
-		parts.add(new Part(6,1,new Dimensions(100,500,500)));
-		parts.add(new Part(7,1,new Dimensions(100,250,250)));
-		parts.add(new Part(8,1,new Dimensions(100,250,250)));
-		parts.add(new Part(9,1,new Dimensions(250,150,250)));
-		parts.add(new Part(10,1,new Dimensions(350,150,350)));
-		parts.add(new Part(11,1,new Dimensions(150,200,200)));
-		parts.add(new Part(12,1,new Dimensions(150,200,200)));
-		parts.add(new Part(13,1,new Dimensions(100,250,250)));
-		parts.add(new Part(14,1,new Dimensions(200,150,200)));
-		parts.add(new Part(15,1,new Dimensions(350,100,350)));
-		Surface surface = Surface.of(1200,800,1000);
-		Layout layout = new Layout(surface, parts);
-		return layout;
+	public static Layout layout12() {
+    return LayoutBuilder.builder()
+      .surface(1200,800,1000)
+      .part(200,400,400)
+      .part(400,400,400)
+      .part(1000,100,1000)
+      .part(200,200,200)
+      .part(200,300,300)
+      .part(100,500,500)
+      .part(100,250,250)
+      .part(100,250,250)
+      .part(250,150,250)
+      .part(350,150,350)
+      .part(150,200,200)
+      .part(150,200,200)
+      .part(100,250,250)
+      .part(200,150,200)
+      .part(350,100,350)
+      .build();
 	}
 
   public static Layout apiExample1() {
@@ -260,25 +243,23 @@ public class LayoutExamples {
     return new Layout(Wagon.of(surfaces), parts);
   }
 
-	/* ----  Impossible solutions ---- */ 
-	
+	/* ----  Impossible solutions ---- */
+
 	// Overlap completely
-	public static Layout unsolvableLayout1(){
-		List<Part> parts = new ArrayList<>();
-		parts.add(new Part(1,1,new Dimensions(10,10,1)));
-		parts.add(new Part(2,1,new Dimensions(10,10,1)));
-		Surface surface = Surface.of(10,10,1);
-		Layout layout = new Layout(surface, parts);
-		return layout;
+	public static Layout unsolvableLayout1() {
+    return LayoutBuilder.builder()
+      .surface(10,10,1)
+      .part(10,10,1)
+      .part(10,10,1)
+      .build();
 	}
-	
+
 	// Fits when looking at area but one part is to wide
-	public static Layout unsolvableLayout2(){
-		List<Part> parts = new ArrayList<>();
-		parts.add(new Part(1,1,new Dimensions(15,5,1)));
-		parts.add(new Part(2,1,new Dimensions(5,5,1)));
-		Surface surface = Surface.of(10,10,1);
-		Layout layout = new Layout(surface, parts);
-		return layout;
+	public static Layout unsolvableLayout2() {
+    return LayoutBuilder.builder()
+      .surface(10,10,1)
+      .part(15,5,1)
+      .part(5,5,1)
+      .build();
 	}
 }
