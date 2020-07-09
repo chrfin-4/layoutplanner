be979437540bd747a4bd67927421838594805e1d
Christoffer Fink <chrfin-4@student.ltu.se>
2020-07-09 09:53:40 +0200

    Fixed bug so it works with multiple surfaces
    In fact, the bug also made surfaces with origin z != 0 not work.

--- src/main/java/se/ltu/kitting/score/ScoreCalculator.java
+++ src/main/java/se/ltu/kitting/score/ScoreCalculator.java
@@ -9,21 +9,20 @@ import se.ltu.kitting.model.Dimensions;
 import java.util.List;
 import ch.rfin.util.Pair;
 
-// FIXME: This is just a dummy place holder.
 public class ScoreCalculator implements EasyScoreCalculator<Layout> {
 
   public HardSoftScore calculateScore(Layout layout) {
 	int hardScore = getHardScore(layout);
     return HardSoftScore.of(hardScore, 0);
   }
-  
+
   // Return negative number of overlapping parts
   public int getHardScore(Layout layout) {
     int outside = countPartsOutside(layout);
 	int overlap = countOverlappingParts(layout);
     return -(overlap + outside);
   }
-  
+
   // Counts the number of parts that overlap eachother
   // NOTE: Counts same overlap multiple times
   public int countOverlappingParts(Layout layout) {
@@ -43,7 +42,7 @@ public class ScoreCalculator implements EasyScoreCalculator<Layout> {
     }
     return count;
   }
-  
+
   // Check if two parts overlap
   public static boolean partsOverlap(Part p1, Part p2) {
 	Pair<Dimensions,Dimensions> currentRegionP1 = p1.currentRegion();
@@ -52,12 +51,12 @@ public class ScoreCalculator implements EasyScoreCalculator<Layout> {
 	Dimensions endPositionP1 = currentRegionP1._2;
 	Dimensions startPositionP2 = currentRegionP2._1;
 	Dimensions endPositionP2 = currentRegionP2._2;
-	
+
 	// Parts do not overlap if they are on different surfaces
 	if(startPositionP1.getZ() != startPositionP2.getZ()){
 	  return false;
 	}
-	
+
 	int rect1xLeft = startPositionP1.getX();
     int rect1xRight = endPositionP1.getX();
     int rect1yBack = startPositionP1.getY();
@@ -65,35 +64,34 @@ public class ScoreCalculator implements EasyScoreCalculator<Layout> {
     int rect2xLeft = startPositionP2.getX();
     int rect2xRight = endPositionP2.getX();
     int rect2yBack = startPositionP2.getY();
-    int rect2yFront = endPositionP2.getY();  
-	  
+    int rect2yFront = endPositionP2.getY();
+
     return rect1xLeft <= rect2xRight && rect1xRight >= rect2xLeft &&
       rect1yBack <= rect2yFront && rect1yFront >= rect2yBack;
   }
-  
+
   // Counts the number of parts outside surface
   public int countPartsOutside(Layout layout){
 	  List<Part> parts = layout.getParts();
-	  Surface surface = layout.getSurface();
 	  int count = 0;
 	  for(Part part : parts) {
 		  if(part.getPosition() == null){
 			  continue;
 		  }
+      Surface surface = layout.surfaceOf(part);
 		  if(partOutside(part, surface)){
 			  count++;
 		  }
 	  }
 	  return count;
   }
-  
+
   // Checks if part is outside of surface
   public boolean partOutside(Part part, Surface surface){
-	Pair<Dimensions,Dimensions> currentRegion = part.currentRegion();
-	boolean height = currentRegion._2.getZ() >= surface.height();
-	boolean depth = currentRegion._2.getY() >= surface.depth();
-	boolean width = currentRegion._2.getX() >= surface.width();
-	return height || depth || width;
+    boolean height = part.height() > surface.height();
+    boolean depth = part.depth() > surface.depth();
+    boolean width = part.width() > surface.width();
+    return height || depth || width;
   }
 
 }
