3207d333f2b42f24ee0652976864d73d19beb00e
Wilma Krutrök <wilkru-7@student.ltu.se>
2020-07-29 10:26:02 +0200

    Added surface id and side from hint to hard score
    If part not placed on specified surface id or side the hard score will
    be negative

--- src/main/java/se/ltu/kitting/score/HardScore.java
+++ src/main/java/se/ltu/kitting/score/HardScore.java
@@ -20,9 +20,9 @@ public class HardScore {
   // Return negative score for parts overlapping, outside or placed on disallowed side
   public static int getHardScore(Layout layout) {
     int outside = countPartsOutside(layout);
-	int overlap = countOverlappingParts(layout);
-	int disallowedSide = countDisallowedSidesDown(layout);
-	int misplacedParts = countMisplacedParts(layout);
+		int overlap = countOverlappingParts(layout);
+		int disallowedSide = countDisallowedSidesDown(layout);
+		int misplacedParts = countMisplacedParts(layout);
     return -(overlap + outside + disallowedSide + misplacedParts);
   }
 
@@ -47,18 +47,18 @@ public class HardScore {
 
   // Check if two parts overlap - considering maximal margin between two parts
   public static boolean partsOverlap(Part p1, Part p2) {
-	Pair<Dimensions,Dimensions> currentRegionP1 = p1.currentRegion();
-	Pair<Dimensions,Dimensions> currentRegionP2 = p2.currentRegion();
-	int margin = Math.max(p1.getMargin(), p2.getMargin());
-	Dimensions startPositionP1 = currentRegionP1._1;
-	Dimensions endPositionP1 = currentRegionP1._2;
-	Dimensions startPositionP2 = currentRegionP2._1;
-	Dimensions endPositionP2 = currentRegionP2._2;
-	// Parts do not overlap if they are on different surfaces
-	if(startPositionP1.getZ() != startPositionP2.getZ()){
-	  return false;
-	}
-	int rect1xLeft = startPositionP1.getX() - margin;
+		Pair<Dimensions,Dimensions> currentRegionP1 = p1.currentRegion();
+		Pair<Dimensions,Dimensions> currentRegionP2 = p2.currentRegion();
+		int margin = Math.max(p1.getMargin(), p2.getMargin());
+		Dimensions startPositionP1 = currentRegionP1._1;
+		Dimensions endPositionP1 = currentRegionP1._2;
+		Dimensions startPositionP2 = currentRegionP2._1;
+		Dimensions endPositionP2 = currentRegionP2._2;
+		// Parts do not overlap if they are on different surfaces
+		if(startPositionP1.getZ() != startPositionP2.getZ()){
+			return false;
+		}
+		int rect1xLeft = startPositionP1.getX() - margin;
     int rect1xRight = endPositionP1.getX() + margin;
     int rect1yBack = startPositionP1.getY() - margin;
     int rect1yFront = endPositionP1.getY() + margin;
@@ -72,16 +72,16 @@ public class HardScore {
 
   // Counts the number of parts outside surface
   public static int countPartsOutside(Layout layout){
-	List<Part> parts = layout.getParts();
-	int count = 0;
-	for(Part part : parts) {
-      if(part.getPosition() != null){
-		if(partOutside(part, layout.surfaceOf(part))){
-		  count++;
+		List<Part> parts = layout.getParts();
+		int count = 0;
+		for(Part part : parts) {
+				if(part.getPosition() != null){
+			if(partOutside(part, layout.surfaceOf(part))){
+				count++;
+			}
+			}
 		}
-	  }
-	}
-	return count;
+		return count;
   }
 
   // Checks if part is outside of surface
@@ -97,54 +97,62 @@ public class HardScore {
   // Count parts placed on a disallowed side
   public static int countDisallowedSidesDown(Layout layout){
     List<Part> parts = layout.getParts();
-	int count = 0;
-	for(Part part : parts) {
-      if(part.getPosition() != null && part.getSideDown() != null){
-	    if(!allowedSideDown(part)){
-		  count++;
-		}
-	  }
-	}	
-	return count;
+		int count = 0;
+		for(Part part : parts) {
+				if(part.getPosition() != null && part.getSideDown() != null){
+				if(!allowedSideDown(part)){
+				count++;
+			}
+			}
+		}	
+		return count;
   }  
 
   // Check if placed side down is allowed
   public static boolean allowedSideDown(Part part){
-	return part.getAllowedDown().contains(part.getSideDown());
+		return part.getAllowedDown().contains(part.getSideDown());
   }		
   
   // Count number of locked parts not at their position
   public static int countMisplacedParts(Layout layout){
     List<Part> parts = layout.getParts();
-	int count = 0;
-	for(Part part : parts) {
-      if(part.getPosition() != null && part.getHint() != null){
-	    if(part.getHint().isMandatory() && !lockedPart(part)){
-		  count++;
-		}
-	  }
-	}	
-	return count;
+		int count = 0;
+		for(Part part : parts) {
+				if(part.getPosition() != null && part.getHint() != null){
+				if(part.getHint().isMandatory() && !lockedPart(layout, part)){
+				count++;
+			}
+			}
+		}	
+		return count;
   }  
   
-  // Check if position or rotation together with position is as specified in hint
-  public static boolean lockedPart(Part part){ 
-	if(!part.getHint().rotation().isPresent()){
-      return lockedPosition(part);
-    } else {
-	  return lockedPosition(part) && lockedRotation(part);
-    }	  
+  // Check if part is places as specified in hint
+  public static boolean lockedPart(Layout layout, Part part){ 
+	  return lockedPosition(layout, part) && lockedRotation(part) && lockedSide(part); 
   }	  
 	  
-  // Check if part has the center position specified in hint
-  public static boolean lockedPosition(Part part){
-	boolean sameX = part.currentCenter().getX() == part.getHint().centerPosition().getX();
-	boolean sameY = part.currentCenter().getY() == part.getHint().centerPosition().getY();
-    return sameX && sameY;
+  // Check if part has the center position and placed on the surface specified in hint
+  public static boolean lockedPosition(Layout layout, Part part){
+	  boolean sameX = part.currentCenter().getX() == part.getHint().centerPosition().getX();
+	  boolean sameY = part.currentCenter().getY() == part.getHint().centerPosition().getY();
+		boolean sameSurface = layout.surfaceOf(part).surfaceId() == part.getHint().surfaceId();
+    return sameX && sameY && sameSurface;
   }
   
+	// Check if part is placed on side specified in hint
+	public static boolean lockedSide(Part part){
+	  if(part.getHint().side().isPresent()){
+			return part.getSideDown().equals(part.getHint().side());
+		}
+		return true;
+	}
+	
   // Check if part has the rotation specified in hint
   public static boolean lockedRotation(Part part){
-    return part.getRotation().equals(part.getHint().rotation().get());
+		if(part.getHint().rotation().isPresent()){
+			return part.getRotation().equals(part.getHint().rotation().get());
+		}
+    return true;
   }
 }
