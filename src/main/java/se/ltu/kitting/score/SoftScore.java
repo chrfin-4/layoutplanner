e014db183b0a9fa866e0ffbbd676a97466700abd
Christoffer Fink <chrfin-4@student.ltu.se>
2020-08-07 05:17:48 +0200

    Fixed hinted side precedence bug
    Hinted side should always take precedence over preferred side, even when
    the layout hint is not mandatory.

--- src/main/java/se/ltu/kitting/score/SoftScore.java
+++ src/main/java/se/ltu/kitting/score/SoftScore.java
@@ -7,7 +7,7 @@ import se.ltu.kitting.model.Part;
 import java.util.List;
 
 /**
- * Calculates the soft score for a solution. 
+ * Calculates the soft score for a solution.
  * Desirable score is 0 which guarantees that:
  * Parts are placed on the preferred side and that
  * parts with a mandatory hint are on the desired position.
@@ -30,45 +30,46 @@ public class SoftScore {
 		for(Part part : parts) {
 			if(part.getPosition() == null || part.getSideDown() == null){
 				continue;
-			}	
+			}
 			if(!preferredSideDown(part)){
 				count++;
 			}
-		}	
+		}
 		return count;
-  }  
+  }
 
   // Check if placed side down is preferred
-  public static boolean preferredSideDown(Part part){
-		if(part.getPreferredDown() == null){
-			return true;
-		}	
-		// If a side is given as a mandatory hint, ignore preferred side
-		if(part.getHint() != null && part.getHint().isMandatory() && part.getHint().side().isPresent()){
-			return true;
-		}
-		return part.getPreferredDown() == part.getSideDown();
-  }		
- 
+  public static boolean preferredSideDown(Part part) {
+    if (part.getPreferredDown() == null){
+      return true;  // Don't have a preference to violate.
+    }
+    // If a side is given as a hint, ignore preferred side.
+    if (part.getHint() != null && part.getHint().side().isPresent()) {
+      return true;
+    }
+    return part.getPreferredDown() == part.getSideDown();
+  }
+
 
   public static int totalDistance(Layout layout){
     List<Part> parts = layout.getParts();
 		int totalDistance = 0;
 		for(Part part : parts) {
 			if(part.getPosition() != null && part.getHint() != null){
+        // TODO: only count this if mandatory?
 				if(part.getHint().isMandatory()){
 					totalDistance += distanceToPosition(part);
 				}
 			}
-		}	
+		}
 		return totalDistance;
-  }  
-  
-	// Distance between parts current postions and mandatory position
-  public static int distanceToPosition(Part part){ 
-		int x = Math.abs(part.currentCenter().getX() - part.getHint().centerPosition().getX());
+  }
+
+  // Distance between part;s current postion and mandatory position.
+  public static int distanceToPosition(Part part) {
+    int x = Math.abs(part.currentCenter().getX() - part.getHint().centerPosition().getX());
     int y = Math.abs(part.currentCenter().getY() - part.getHint().centerPosition().getY());
     return x + y;
-  }	
-	
+  }
+
 }
